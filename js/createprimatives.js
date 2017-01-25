f14a863bc4ee12d0f24da86d577784d849f890a6
Brent Shambaugh <brent.shambaugh@gmail.com>
2017-01-25 15:56:41 -0600

    first call to lodash

--- js/createprimatives.js
+++ js/createprimatives.js
@@ -4,8 +4,9 @@ define(['lodash'], function(_) {
  
 var a = [1,2,3,4,5];
 document.write(a);
-//var b = _.join(a,',');
-//document.write(b);
+var b = _.join(a,',');
+document.write('<br>');
+document.write(b);
 //document.write(_.join(_.reverse(a),','));
 
 //document.write(add(3,4));
