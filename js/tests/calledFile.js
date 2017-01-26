69588aab903b3ebf31b3a0277cc061338872040a
Brent Shambaugh <brent.shambaugh@gmail.com>
2017-01-25 20:31:53 -0600

    added a called and calling file in the tests following Rob Dodsons RequireJS -- Embracing the Awesomeness of Asynchronous Modules on YouTube

---
+++ js/tests/calledFile.js
@@ -0,0 +1,10 @@
+// calledFile.js
+define(function() {
+  
+  return {
+    name: 'Rob',
+    sayHello: function() {
+      alert('Hi, my name is ' + this.name);
+    }
+  }
+});
---
+++ js/tests/callingFile.js
@@ -0,0 +1,4 @@
+// callingFile.js
+define(['tests/calledFile'], function(calledFile) {
+  calledFile.sayHello(); // alerts 'Hi, my name is Rob'
+});
