c4c33088335e87ed0f23115862d340e0cd5ca640
Brent Shambaugh <brent.shambaugh@gmail.com>
2017-01-26 09:46:35 -0600

    Changed sketch to draw node edge graph

--- js/sketch.js
+++ js/sketch.js
@@ -17,7 +17,7 @@ function processing.setup() {
 }
 */
 
-var s = function( p ) {
+var sketch = function( p ) {
 
    var x = 100;
    var y = 100;
@@ -29,42 +29,25 @@ var s = function( p ) {
    p.draw = function() {
      p.background(0);
      p.fill(255);
-     p.rect(x,y,50,50);
+     p.stroke(255);
+
+     p.line(nums[0][0], nums[0][1], nums[1][0], nums[1][1]);
+     p.line(nums[1][0],nums[1][1], nums[2][0], nums[2][1]);
+
+     p.ellipse(nums[0][0], nums[0][1],radius, radius);
+     p.text(titles[0], nums[0][0] - titles[0].length - tw, nums[0][1] + 0.5*tw, tw);
+
+     p.ellipse(nums[1][0], nums[1][1], radius, radius);
+     p.text(titles[1], nums[1][0] - titles[1].length - tw, nums[1][1] + 0.5*tw, tw);
+
+     p.ellipse(nums[2][0],nums[2][1], radius, radius);
+     p.text(titles[2],nums[2][0] - titles[2].length - tw, nums[2][1] + 0.5*tw, tw);
+
    };
 }; 
 
-var myp5 = new p5(s);
+var myp5 = new p5(sketch);
 
-/*
-function setup() {
-   createCanvas(400,400);
-}
-*/
-
-/*
-function processing.setup() {
-
-}
-*/
-
-/*
-function draw() {
- line(nums[0][0], nums[0][1], nums[1][0], nums[1][1]);
- line(nums[1][0],nums[1][1], nums[2][0], nums[2][1]);
-
-  ellipse(nums[0][0], nums[0][1],radius, radius);
-  text(titles[0], nums[0][0] - titles[0].length - tw, nums[0][1] + 0.5*tw, tw);
-
-//  line(nums[1][0],nums[1][1], nums[2][0], nums[2][1]);
-  ellipse(nums[1][0], nums[1][1], radius, radius);
-  text(titles[1], nums[1][0] - titles[1].length - tw, nums[1][1] + 0.5*tw, tw);
-
-  ellipse(nums[2][0],nums[2][1], radius, radius);
-  text(titles[2],nums[2][0] - titles[2].length - tw, nums[2][1] + 0.5*tw, tw);
-  
-//  line(nums[1][0],nums[1][1], nums[2][0], nums[2][1]);
-}
-*/
 
 });
 
--- js/test.js
+++
@@ -1,4 +0,0 @@
-// callingFile.js
-define(['createprimatives'], function(createprimatives) {
-  document.write(createprimatives.createprimatives.nodes[0].x); // alerts 'Hi, my name is Rob'
-});
