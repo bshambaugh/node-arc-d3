04f970ea6997a0591af9ca4abc8bcf747025fc29
Brent Shambaugh <brent.shambaugh@gmail.com>
2017-01-25 21:59:40 -0600

    Moved the test to call createprimatives to tests and renamed to call-createprimatives-test

---
+++ js/tests/call-createprimatives-test.js
@@ -0,0 +1,4 @@
+// call-createprimatives-test.js
+define(['../createprimatives'], function(createprimatives) {
+  document.write(createprimatives.createprimatives.nodes[0].x); // alerts 'Hi, my name is Rob'
+});
