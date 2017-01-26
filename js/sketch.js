514b545cee13d018a2ea575c5fbef6288b91c85c
Brent Shambaugh <brent.shambaugh@gmail.com>
2017-01-26 14:48:03 -0600

    I added edge labels

--- js/sketch.js
+++ js/sketch.js
@@ -1,52 +1,22 @@
 define(['p5','p5dom','createprimatives'], function(p5,p5dom,createprimatives) {
 //var nums = [[56, 45],[185, 175],[185, 45]];
-var nums = [];
-var arredges = [];
 var radius = 55;
 var tw = 10;
-var titles = ["Hello","World","!"];
+//var titles = ["Hello","World","!"];
 
-//var numbers = [];
+var Primatives = createprimatives.createprimatives;
 
-for(var i = 0; i < createprimatives.createprimatives.nodes.length; i++) {
-  var x = createprimatives.createprimatives.nodes[i].x;
-  //document.write(createprimatives.createprimatives.nodes[i].x);
-  // document.write(' , ');
-  //document.write(createprimatives.createprimatives.nodes[i].y);
-  var y = createprimatives.createprimatives.nodes[i].y;
-  //document.write('<br><br>');
-//  numbers.push([x , y]);
-  nums.push([x , y]);
-}
-
-for(var i = 0; i < nums.length; i++) {
-// document.write(nums[i]);
-// document.write('<br>');
-}
-
-for(var i = 0; i < createprimatives.createprimatives.edges.length; i++) {
-  var point1 = createprimatives.createprimatives.edges[i].point1;
-  var point2 = createprimatives.createprimatives.edges[i].point2;  
-  var title =  createprimatives.createprimatives.edges[i].label_and_predicate_name;
- // document.write(point1[0] + ' , ' + point1[1] + ' : ' + point2[0] + ' , ' +  point2[1] +  ' : ' + title);
- // document.write('<br>');
- arredges.push(point1[0],point1[1],point2[0],point2[1]);
-}
-
-for(var i = 0; i < arredges.length; i++) {
-  document.write(arredges[i][0] + ' : ' + arredges[i][1] + ' : ' + arredges[i][2] + ' : ' + arredges[i][3]);
-}
 /*
-document.write(numbers);
-document.write('<br>');
-for(var i = 0; i < numbers.length; i++) {
- document.write(numbers[i][1]);
- document.write('<br>');
+for(var i = 0; i < Primatives.nodes.length; i++) {
+   document.write(Primatives.nodes[i].x + ' , ' + Primatives.nodes[i].y + ' , ' + Primatives.nodes[i].node_name );
+   document.write('<br>');
 }
 
-for(var i = 0; i < nums.length; i++) {
- document.write(nums[i][0]);
- document.write('<br>');
+document.write('<br>');
+
+for(var i = 0; i < Primatives.edges.length; i++) {
+document.write(Primatives.edges[i].point1[0] + ' , ' + Primatives.edges[i].point1[1] + ' : ' + Primatives.edges[i].point2[0] + ' , ' +Primatives.edges[i].point2[1]  +' : ' + Primatives.edges[i].label_and_predicate_name);
+document.write('<br>');
 }
 */
 
@@ -64,38 +34,24 @@ var sketch = function( p ) {
      p.fill(255);
      p.stroke(255);
 
-  for(var i = 0; i < arredges.length; i++) {
-    p.line(arredges[i][0],arredges[i][1],arredges[i][2],arredges[i][3]);
-  } 
-
-/*
-    for(var i = 0; i < createprimatives.createprimatives.edges.length; i++) {
-      p.line(createprimatives.createprimatives.edges[i].point1[0],createprimatives.createprimatives.edges[i].point1[1],createprimatives.createprimatives.edges[i].point2[1],createprimatives.createprimatives.edges[i].point2[1]);
-    }
-*/
-
-/*
-     for(var i = 0; i < nums.length - 1; i++) {
-        p.line(nums[i][0], nums[i][1], nums[i+1][0], nums[i+1][1]);
-     }
-*/
-
-    for(var i = 0; i < createprimatives.createprimatives.nodes.length; i++) {
-       p.ellipse(createprimatives.createprimatives.nodes[i].x, createprimatives.createprimatives.nodes[i].y, radius, radius);
+  for(var i = 0; i < Primatives.edges.length; i++) {
+//    p.line(arredges[i][0],arredges[i][1],arredges[i][2],arredges[i][3]);
+      p.line(Primatives.edges[i].point1[0],Primatives.edges[i].point1[1],Primatives.edges[i].point2[0],Primatives.edges[i].point2[1]);
+    //  p.ellipse(0.5 * (Primatives.edges[i].point2[0] + Primatives.edges[i].point1[0]),0.5 * (Primatives.edges[i].point2[1] + Primatives.edges[i].point1[1]),5,5); 
+    //  p.line(Primatives.edges[i].point1[0],Primatives.edges[i].point1[1]+10,Primatives.edges[i].point2[0],Primatives.edges[i].point2[1]+10);
+       //    p.ellipse(0.5 * (Primatives.edges[i].point2[0] + Primatives.edges[i].point1[0]),0.5 * (Primatives.edges[i].point2[1]+10 + Primatives.edges[i].point1[1]+10),5,5);
+     p.text(Primatives.edges[i].label_and_predicate_name,0.5 * (Primatives.edges[i].point2[0] + Primatives.edges[i].point1[0]),0.5 * (Primatives.edges[i].point2[1]+tw + Primatives.edges[i].point1[1]+tw),tw + 5,tw + 5);
+  }
+ 
+//    for(var i = 0; i < createprimatives.createprimatives.nodes.length; i++) {
+  for(var i = 0; i < Primatives.nodes.length; i++) {
+ //      p.ellipse(createprimatives.createprimatives.nodes[i].x, createprimatives.createprimatives.nodes[i].y, radius, radius);
+       p.ellipse(Primatives.nodes[i].x,Primatives.nodes[i].y, radius, radius);
        p.fill(0);
-       p.text(createprimatives.createprimatives.nodes[i].node_name, createprimatives.createprimatives.nodes[i].x - createprimatives.createprimatives.nodes[i].node_name.length - tw, createprimatives.createprimatives.nodes[i].y + 0.5*tw, tw);
+       p.text(Primatives.nodes[i].node_name, Primatives.nodes[i].x - Primatives.nodes[i].node_name.length - tw, Primatives.nodes[i].y + 0.5*tw, tw);
+   //    p.text(createprimatives.createprimatives.nodes[i].node_name, createprimatives.createprimatives.nodes[i].x - createprimatives.createprimatives.nodes[i].node_name.length - tw, createprimatives.createprimatives.nodes[i].y + 0.5*tw, tw);
        p.fill(255);
     }
-
-/*
-     for(var i = 0; i < nums.length; i++) {
-        p.ellipse(nums[i][0], nums[i][1],radius, radius);
-        p.fill(0);
-        p.text(titles[i], nums[i][0] - titles[i].length - tw, nums[i][1] + 0.5*tw, tw);
-        p.fill(255);
-     }
-
-*/
 
    };
 
