ab32cc0d66de4ab6904ca77f3099205882e06c71
Brent Shambaugh <brent.shambaugh@gmail.com>
2017-01-26 08:48:45 -0600

    Created a sketch that works by instance mode and moved it to tests see : https://github.com/processing/p5.js/wiki/p5.js-overview#instantiation--namespace

--- js/config.js
+++ js/config.js
@@ -3,6 +3,8 @@
 requirejs.config({
     baseUrl : 'js',
     paths : {
-       lodash : 'libraries/lodash.min'
+       lodash : 'libraries/lodash.min',
+       p5: 'libraries/p5',
+       p5dom: 'libraries/p5.dom' 
     }
 });
