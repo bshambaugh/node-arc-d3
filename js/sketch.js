c4d2d7c71ac5f93986112fefedbfbe6af4d29084
Brent Shambaugh <brent.shambaugh@gmail.com>
2017-01-26 10:43:25 -0600

    removed commented out code to minfy

--- js/sketch.js
+++ js/sketch.js
@@ -4,19 +4,6 @@ var radius = 55;
 var tw = 10;
 var titles = ["Hello","World","!"];
 
-//document.write('Hello world!');
-
-//document.write(p5);
-/*
-var processing = new p5();
-document.write(processing);
-*/
-/*
-function processing.setup() {
-  createCanvas(400,400);
-}
-*/
-
 var sketch = function( p ) {
 
    var x = 100;
@@ -42,27 +29,9 @@ var sketch = function( p ) {
         p.text(titles[i], nums[i][0] - titles[i].length - tw, nums[i][1] + 0.5*tw, tw);
         p.fill(255);
      }
-/*     
-      for(var i = 0; i < nums.length; i++) {
-         p.ellipse(nums[i][0], nums[i][1],radius, radius);
-         p.text(titles[i], nums[i][0] - titles[i].length - tw, nums[i][1] + 0.5*tw, tw);
-      }
-
-
-     p.line(nums[0][0], nums[0][1], nums[1][0], nums[1][1]);
-     p.line(nums[1][0],nums[1][1], nums[2][0], nums[2][1]);
-
-     p.ellipse(nums[0][0], nums[0][1],radius, radius);
-     p.text(titles[0], nums[0][0] - titles[0].length - tw, nums[0][1] + 0.5*tw, tw);
-
-     p.ellipse(nums[1][0], nums[1][1], radius, radius);
-     p.text(titles[1], nums[1][0] - titles[1].length - tw, nums[1][1] + 0.5*tw, tw);
-
-     p.ellipse(nums[2][0],nums[2][1], radius, radius);
-     p.text(titles[2],nums[2][0] - titles[2].length - tw, nums[2][1] + 0.5*tw, tw);
-*/
 
    };
+
 }; 
 
 var myp5 = new p5(sketch);
@@ -70,10 +39,3 @@ var myp5 = new p5(sketch);
 
 });
 
-/*
-for (i = 0; i < nums.length; i++) {
-   for (j = 0; j < nums.length - 1; j++) {
-      document.write(nums[i][j] + ' ' + nums[i][j+1] + ' ' + nums[i+1][j] + ' ' + nums[i+1][j+1]);
-   }
-} 
-*/
