824e60da0b6e8fa81fdc4c807ce5366ab711c19e
Brent Shambaugh <brent.shambaugh@gmail.com>
2017-03-19 17:33:02 -0500

    Modified flowchart for programmed and prepped sparql and ldp http requests for amd

---
+++ js/tests/jquerysparql/index3.js
@@ -0,0 +1,28 @@
+define(['lodash','n3lib','jquery'], function(_,N3,$) {
+
+  var siteDomain = "localhost:8080/marmotta/sparql?query=";
+  var query = "SELECT * WHERE {?s ?p ?o} LIMIT 14";
+  var url = "http://" + siteDomain;
+  url += encodeURIComponent(query);
+  let triples = [];
+ // document.write(url);
+  $.ajax({
+	dataType: 'json',
+	url: url,
+	success: function(data) {
+	//	alert('success: ' + data.results.bindings.length + ' results');
+	          //		console.log(data.results.bindings.length);
+                       for(var k = 0; k < data.results.bindings.length; k++) {
+                           let subject = data.results.bindings[k].s.value;
+                          //  let subject = JSON.stringify(data.results.bindings[k].s.value);
+                         //   let predicate = JSON.stringify(data.results.bindings[k].p.value);
+                            let predicate = data.results.bindings[k].p.value;
+                            let object = data.results.bindings[k].o.value;
+                          //  let object = JSON.stringify(data.results.bindings[k].o.value);
+                            triples.push([subject,predicate,object]);
+                         }
+                   document.write(triples);
+	      	}
+  });
+
+});
