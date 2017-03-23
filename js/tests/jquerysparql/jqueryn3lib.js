9ef7a2b56669556b968a8ba8c10829507570f936
Brent Shambaugh <brent.shambaugh@gmail.com>
2017-03-23 17:02:48 -0500

    added jquery sparql code

--- js/tests/jquerysparql/jqueryn3lib.js
+++ js/tests/jquerysparql/jqueryn3lib.js
@@ -1,17 +1,25 @@
-define(['n3lib','jquery'], function(N3,$) {
+define(['jquery','n3lib'], function($,N3) {
 
-  var siteDomain = "localhost:8080/marmotta/sparql?query=";
-  var query = "SELECT * WHERE {?s ?p ?o} LIMIT 14";
-  var url = "http://" + siteDomain;
-  url += encodeURIComponent(query);
-  let triples = [];
- // document.write(url);
-  $.ajax({
-	dataType: 'json',
-	url: url,
-	success: function(data) {
-	//	alert('success: ' + data.results.bindings.length + ' results');
-	          //		console.log(data.results.bindings.length);
+var string = '';
+
+   jquerysparql(string, function(duck) {
+
+
+   });
+
+   function jquerysparql(string, fn) {
+     var siteDomain = "localhost:8080/marmotta/sparql?query=";
+     var query = "SELECT * WHERE {?s ?p ?o} LIMIT 14";
+     var url = "http://" + siteDomain;
+     url += encodeURIComponent(query);
+     let triples = [];
+     // document.write(url);
+     $.ajax({
+        dataType: 'json',
+        url: url,
+        success: function(data) {
+        //      alert('success: ' + data.results.bindings.length + ' results');
+                  //            console.log(data.results.bindings.length);
                        for(var k = 0; k < data.results.bindings.length; k++) {
                            let subject = data.results.bindings[k].s.value;
                           //  let subject = JSON.stringify(data.results.bindings[k].s.value);
@@ -22,7 +30,11 @@ define(['n3lib','jquery'], function(N3,$) {
                             triples.push([subject,predicate,object]);
                          }
                    document.write(triples);
-	      	}
-  });
+                   fn(triples);
+                }
+     });
+
+  }
+
 
 });
